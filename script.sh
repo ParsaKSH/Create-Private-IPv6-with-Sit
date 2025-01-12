5942eb2873e616e1ddb11479b30d5a3f99778ec4
Parsa <142115135+bluetiger4511@users.noreply.github.com>
2025-01-12 22:51:11 +0330

    script.sh

--- script.sh
+++ script.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-echo "=========================================="
-echo "Created by Parsa in OPIran club"
-echo "Love Iran :)"
-echo "=========================================="
+# ==========================================
+# Created by Parsa in OPIran club
+# Love Iran :)
+# ==========================================
+echo -e "\033[1;33m=========================================="
+echo -e "Created by Parsa in OPIran club"
+echo -e "Love Iran :)"
+echo -e "==========================================\033[0m"
 
 read -p "Please enter the IPv4 address of the Iran server: " iran_ip
 read -p "Please enter the IPv4 address of the foreign server: " foreign_ip
 read -p "Are you running this script on the Iran server? (yes/no): " server_location
 
-echo "Updating and installing required packages..."
+echo -e "\033[1;33mUpdating and installing required packages...\033[0m"
 sudo apt update
 sudo apt-get install iproute2 -y
+sudo apt install nano -y
 sudo apt install netplan.io -y
 
 if [ "$server_location" == "yes" ]; then
-    echo "Configuring for the Iran server..."
+    echo -e "\033[1;33mConfiguring for the Iran server...\033[0m"
     sudo bash -c "cat > /etc/netplan/pdtun.yaml <<EOF
 network:
   version: 2
@@ -37,10 +42,10 @@ Address=2619:db8:69a3:1b2e::2/64
 Gateway=2619:db8:69a3:1b2e::1
 EOF"
 
-    echo "This is your Private-IPv6 for your Iran server: 2619:db8:69a3:1b2e::2/64"
+    echo -e "\033[1;31mThis is your Private-IPv6 for your Iran server: 2619:db8:69a3:1b2e::2/64\033[0m"
 
 else
-    echo "Configuring for the foreign server..."
+    echo -e "\033[1;33mConfiguring for the foreign server...\033[0m"
     sudo bash -c "cat > /etc/netplan/pdtun.yaml <<EOF
 network:
   version: 2
@@ -62,15 +67,15 @@ Address=2619:db8:69a3:1b2e::1/64
 Gateway=2619:db8:69a3:1b2e::2
 EOF"
 
-    echo "This is your Private-IPv6 for your foreign server: 2619:db8:69a3:1b2e::1/64"
+    echo -e "\033[1;31mThis is your Private-IPv6 for your foreign server: 2619:db8:69a3:1b2e::1/64\033[0m"
 fi
 
 sudo systemctl restart systemd-networkd
 
 read -p "Operation completed successfully. Please reboot the system. Would you like to reboot the system? (yes/no): " reboot_choice
 if [ "$reboot_choice" == "yes" ]; then
-    echo "Rebooting the system..."
+    echo -e "\033[1;33mRebooting the system...\033[0m"
     sudo reboot
 else
-    echo "Operation completed successfully. Reboot required."
+    echo -e "\033[1;33mOperation completed successfully. Reboot required.\033[0m"
 fi
