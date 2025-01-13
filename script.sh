1305512addc5ea546d659cacd8afa18211855b8b
Parsa <142115135+ParsaKSH@users.noreply.github.com>
2025-01-13 11:10:59 +0330

    script.sh

--- script.sh
+++ script.sh
@@ -7,8 +7,8 @@ echo -e "==========================================\033[0m"
 
 read -p "Please enter the IPv4 address of the Iran server: " iran_ip
 read -p "Please enter the IPv4 address of the foreign server: " foreign_ip
-read -p "Please enter the MTU (press Enter for default 1380): " mtu
-mtu=${mtu:-1380}
+read -p "Please enter the MTU (press Enter for default 1420): " mtu
+mtu=${mtu:-1420}
 
 function ask_yes_no() {
     local prompt=$1
