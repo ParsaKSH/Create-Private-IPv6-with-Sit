5febe345df3b30b09f75c86ce10a1eeaea56c4a4
Parsa <142115135+ParsaKSH@users.noreply.github.com>
2025-01-15 01:57:46 +0100

    Multi server added! script.sh

--- script.sh
+++ script.sh
@@ -5,10 +5,12 @@ echo -e "Created by Parsa in OPIran club https://t.me/OPIranClub"
 echo -e "Love Iran :)"
 echo -e "==========================================\033[0m"
 
-read -p "Please enter the IPv4 address of the Iran server: " iran_ip
-read -p "Please enter the IPv4 address of the foreign server: " foreign_ip
-read -p "Please enter the MTU (press Enter for default 1420): " mtu
-mtu=${mtu:-1420}
+read -p "Are you running this script on the IRAN server or the FOREIGN server? (IRAN/FOREIGN): " server_location_en
+echo -e "\033[1;33mUpdating and installing required packages...\033[0m"
+sudo apt update
+sudo apt-get install iproute2 -y
+sudo apt install nano -y
+sudo apt install netplan.io -y
 
 function ask_yes_no() {
     local prompt=$1
@@ -24,44 +26,94 @@ function ask_yes_no() {
     done
 }
 
-server_location=$(ask_yes_no "Are you running this script on the Iran server?")
+if [[ "$server_location_en" == "IRAN" || "$server_location_en" == "iran" ]]; then
+    read -p "Please enter the IPv4 address of the IRAN server: " iran_ip
+    read -p "Please enter the MTU (press Enter for default 1420): " mtu
+    mtu=${mtu:-1420}
+    read -p "How many FOREIGN servers do you have? " n_server
+    declare -a foreign_ips
+    for (( i=1; i<=$n_server; i++ )); do
+        read -p "Enter IPv4 of FOREIGN server #$i: " temp_ip
+        foreign_ips[i]=$temp_ip
+    done
 
-echo -e "\033[1;33mUpdating and installing required packages...\033[0m"
-sudo apt update
-sudo apt-get install iproute2 -y
-sudo apt install nano -y
-sudo apt install netplan.io -y
-
-if [ "$server_location" == "yes" ]; then
-    echo -e "\033[1;33mConfiguring for the Iran server...\033[0m"
-    sudo bash -c "cat > /etc/netplan/pdtun.yaml <<EOF
+    for (( i=1; i<=$n_server; i++ )); do
+        if (( i % 2 == 1 )); then
+            y=$i
+        else
+            y=$((i+1))
+        fi
+        netplan_file="/etc/netplan/pdtun${i}.yaml"
+        tunnel_name="tunel0$y"
+        sudo bash -c "cat > $netplan_file <<EOF
 network:
   version: 2
   tunnels:
-    tunel01:
+    $tunnel_name:
       mode: sit
       local: $iran_ip
-      remote: $foreign_ip
+      remote: ${foreign_ips[i]}
       addresses:
-        - 2619:db8:85a3:1b2e::2/64
+        - 2619:db8:85a3:1b2e::$((2*i))/64
       mtu: $mtu
 EOF"
-
-    sudo netplan apply
-    sudo systemctl unmask systemd-networkd.service
-    sudo systemctl start systemd-networkd
-    sudo netplan apply
-
-    sudo bash -c "cat > /etc/systemd/network/tun0.network <<EOF
+        sudo netplan apply
+        sudo systemctl unmask systemd-networkd.service
+        sudo systemctl start systemd-networkd
+        sudo netplan apply
+        network_file="/etc/systemd/network/tun${i}.network"
+        sudo bash -c "cat > $network_file <<EOF
 [Network]
-Address=2619:db8:85a3:1b2e::2/64
-Gateway=2619:db8:85a3:1b2e::1
+Address=2619:db8:85a3:1b2e::$((2*i))/64
+Gateway=2619:db8:85a3:1b2e::$((2*i - 1))
 EOF"
+        echo -e "\033[1;37mThis is your Private-IPv6 for IRAN server #$i: 2619:db8:85a3:1b2e::$((2*i))\033[0m"
+    done
+    sudo sysctl -w net.ipv6.conf.all.forwarding=1
 
-    echo -e "\033[1;37mThis is your Private-IPv6 for your Iran server: 2619:db8:85a3:1b2e::2\033[0m"
-
+    
+    sudo ip link set tunel01 up
+    sudo ip -6 route replace 2619:db8:85a3:1b2e::1/128 dev tunel01
+    sudo ip link set tunel03 up
+    sudo ip -6 route replace 2619:db8:85a3:1b2e::3/128 dev tunel03
+    sudo ip link set tunel05 up
+    sudo ip -6 route replace 2619:db8:85a3:1b2e::5/128 dev tunel05
+    sudo ip link set tunel07 up
+    sudo ip -6 route replace 2619:db8:85a3:1b2e::7/128 dev tunel07
+    sudo ip link set tunel09 up
+    sudo ip -6 route replace 2619:db8:85a3:1b2e::9/128 dev tunel09
+    sudo ip link set tunel011 up
+    sudo ip -6 route replace 2619:db8:85a3:1b2e::11/128 dev tunel011
+    sudo ip link set tunel013 up
+    sudo ip -6 route replace 2619:db8:85a3:1b2e::13/128 dev tunel013
+    sudo ip link set tunel015 up
+    sudo ip -6 route replace 2619:db8:85a3:1b2e::15/128 dev tunel015
+    sudo ip link set tunel017 up
+    sudo ip -6 route replace 2619:db8:85a3:1b2e::17/128 dev tunel017
+    sudo ip link set tunel019 up
+    sudo ip -6 route replace 2619:db8:85a3:1b2e::19/128 dev tunel019
+    sudo ip link set tunel021 up
+    sudo ip -6 route replace 2619:db8:85a3:1b2e::21/128 dev tunel021
+    
+    sudo systemctl restart systemd-networkd
+    reboot_choice=$(ask_yes_no "Operation completed successfully. Please reboot the system")
+    if [ "$reboot_choice" == "yes" ]; then
+        echo -e "\033[1;33mRebooting the system...\033[0m"
+        sudo reboot
+    else
+        echo -e "\033[1;33mOperation completed successfully. Reboot required.\033[0m"
+    fi
 else
-    echo -e "\033[1;33mConfiguring for the foreign server...\033[0m"
+    read -p "Please enter the IPv4 address of the FOREIGN server: " foreign_ip
+    read -p "Please enter the IPv4 address of the IRAN server: " iran_ip
+    read -p "Please enter the MTU (press Enter for default 1420): " mtu
+    mtu=${mtu:-1420}
+    read -p "Which number is this FOREIGN server? (If you have multiple foreign servers, type which one this is. If only one, type 1): " server_number
+    if (( server_number % 2 == 0 )); then
+        this_server=$((server_number + 1))
+    else
+        this_server=$server_number
+    fi
     sudo bash -c "cat > /etc/netplan/pdtun.yaml <<EOF
 network:
   version: 2
@@ -71,30 +123,28 @@ network:
       local: $foreign_ip
       remote: $iran_ip
       addresses:
-        - 2619:db8:85a3:1b2e::1/64
+        - 2619:db8:85a3:1b2e::$this_server/64
       mtu: $mtu
 EOF"
-
     sudo netplan apply
     sudo systemctl unmask systemd-networkd.service
     sudo systemctl start systemd-networkd
     sudo netplan apply
-
+    gateway_for_foreign=$((this_server + 1))
     sudo bash -c "cat > /etc/systemd/network/tun0.network <<EOF
 [Network]
-Address=2619:db8:85a3:1b2e::1/64
-Gateway=2619:db8:85a3:1b2e::2
+Address=2619:db8:85a3:1b2e::$this_server/64
+Gateway=2619:db8:85a3:1b2e::$gateway_for_foreign
 EOF"
-
-    echo -e "\033[1;37mThis is your Private-IPv6 for your foreign server: 2619:db8:85a3:1b2e::1\033[0m"
-fi
-
-sudo systemctl restart systemd-networkd
-
-reboot_choice=$(ask_yes_no "Operation completed successfully. Please reboot the system")
-if [ "$reboot_choice" == "yes" ]; then
-    echo -e "\033[1;33mRebooting the system...\033[0m"
-    sudo reboot
-else
-    echo -e "\033[1;33mOperation completed successfully. Reboot required.\033[0m"
+    echo -e "\033[1;37mThis is your Private-IPv6 for your FOREIGN server: 2619:db8:85a3:1b2e::$this_server\033[0m"
+    sudo systemctl restart systemd-networkd
+    sudo ip link set "tunel0$this_server" up
+    sudo ip -6 route replace 2619:db8:85a3:1b2e::$this_server/128 dev "tunel0$this_server"
+    reboot_choice=$(ask_yes_no "Operation completed successfully. Please reboot the system")
+    if [ "$reboot_choice" == "yes" ]; then
+        echo -e "\033[1;33mRebooting the system...\033[0m"
+        sudo reboot
+    else
+        echo -e "\033[1;33mOperation completed successfully. Reboot required.\033[0m"
+    fi
 fi
